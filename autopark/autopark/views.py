82a666c798acb35e265100905ecded06c922b4cc
Abdiel Bonilla <abboro123@gmail.com>
2025-02-26 20:20:05 -0600

    Creación página 5 y modificaciones a la pagina 3

--- autopark/autopark/views.py
+++ autopark/autopark/views.py
@@ -1,10 +1,26 @@
 from django.http import HttpResponse
 from django.template import Template, Context
 
+def hello (request):
+    return HttpResponse("Hola Mundo")
 
-def home(request):
+
+def page3(request):
     #abrimos un docuemnto que contiene la plantilla html
-    plantillaExterna=open("C:/Users/Usuario/Desktop/autopark/integradora/templates/home.html")
+    plantillaExterna=open("autopark/templates/pagina3.html")
+    #cargar el documento en una variable de tipo Template
+    templates=Template(plantillaExterna.read())
+    #cerramos el documento
+    plantillaExterna.close()
+    #creamos un contexto
+    contexto=Context()
+    #renderizamos la plantilla
+    docuemnto = templates.render(contexto)
+    return HttpResponse(docuemnto)
+
+def page5(request):
+    #abrimos un docuemnto que contiene la plantilla html
+    plantillaExterna=open("autopark/templates/pagina5.html")
     #cargar el documento en una variable de tipo Template
     templates=Template(plantillaExterna.read())
     #cerramos el documento
